208e64901be39917e495e17212c20c07455e458d
Alek Evans <contact@alekeagle.com>
2021-01-12 20:22:20 -0700

    update copywrong

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,22 @@
 <template>
-    <div ref="footerBox" class="footer_box">
-        <slot />
-        <div class="cpyrit">
-            <a href="https://alekeagle.com" class="footer_text"
-                >© 2019-2020 AlekEagle</a
-            >
-        </div>
+  <div ref="footerBox" class="footer_box">
+    <slot />
+    <div class="cpyrit">
+      <a href="https://alekeagle.com" class="footer_text"
+        >© 2019-2021 AlekEagle</a
+      >
     </div>
+  </div>
 </template>
 
 <script>
-export default {
+  export default {
     name: 'Footer'
-};
+  };
 </script>
 
 <style>
-.footer_text {
+  .footer_text {
     color: #a8a8a8;
     font-size: 20px;
     margin-bottom: 0px;
@@ -26,20 +26,20 @@ export default {
     text-decoration: none;
     transition-property: color;
     transition-duration: 0.3s;
-}
+  }
 
-.footer_text:hover {
+  .footer_text:hover {
     transition-property: color;
     transition-duration: 0.3s;
     color: #1199ff;
-}
+  }
 
-.footer_box {
+  .footer_box {
     width: 100%;
     position: relative;
     float: left;
     z-index: 1;
     margin-top: 10px;
     margin-bottom: 10px;
-}
+  }
 </style>
